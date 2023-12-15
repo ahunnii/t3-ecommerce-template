1a1a0570b73ff3d2efec7c4933e94213dc481f67
Andrew Hunn <ahunn59@gmail.com>
2023-12-15 13:05:56 -0500

    (chore) walk back dynamic pages in favor of organizational forks

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,23 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import dynamic from "next/dynamic";
 import Head from "next/head";
-import getProducts from "~/actions/core/get-products";
-import ProductList from "~/components/core/product/product-list";
-import GalleryCard from "~/components/core/ui/gallery-card";
-import { env } from "~/env.mjs";
 
+import GalleryCard from "~/components/core/ui/gallery-card";
 import StorefrontLayout from "~/layouts/StorefrontLayout";
+
 import { api } from "~/utils/api";
 
-const DynamicGalleryPage = dynamic(
-  () =>
-    import(`~/components/${env.NEXT_PUBLIC_SITE_DIRECTORY}/pages/GalleryPage`),
-  {
-    ssr: false,
-  }
-);
-
 const GalleryPage = () => {
+  const { data: products } = api.products.getAllProducts.useQuery({});
+
+  const items = products?.map((product) => product.images[0]?.url) ?? [];
+
   return (
     <>
       <Head>
@@ -25,7 +17,20 @@ const GalleryPage = () => {
         <meta name="description" content="Admin" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DynamicGalleryPage />
+      <StorefrontLayout>
+        <div className="space-y-10 py-10">
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <div className="space-y-4">
+              <h3 className="text-3xl font-bold">Gallery</h3>
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {items.map((item, idx) => (
+                  <GalleryCard key={idx} data={item!} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </StorefrontLayout>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,40 @@
-import { useQuery } from "@tanstack/react-query";
-import { GetServerSidePropsContext } from "next";
-import dynamic from "next/dynamic";
 import Head from "next/head";
-import { FC } from "react";
-import getProducts from "~/actions/core/get-products";
-import ProductList from "~/components/core/product/product-list";
-import { env } from "~/env.mjs";
 
+import ProductList from "~/components/core/product/product-list";
 import StorefrontLayout from "~/layouts/StorefrontLayout";
 
 import { api } from "~/utils/api";
 
-const DynamicHomePage = dynamic(
-  () => import(`~/components/${env.NEXT_PUBLIC_SITE_DIRECTORY}/pages/HomePage`),
-  {
-    ssr: false,
-  }
-);
-
 const HomePage = () => {
+  const { data: products, isLoading } = api.products.getAllProducts.useQuery({
+    isFeatured: true,
+  });
   return (
     <>
       <Head>
-        <title>Homepage | DreamWalker Studios</title>
+        <title>Homepage | Store Co.</title>
         <meta name="description" content="Admin" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <DynamicHomePage />
+      <StorefrontLayout>
+        <div className="space-y-10 pb-10">
+          <div className="overflow-hidden rounded-xl p-4 sm:p-6 lg:p-8">
+            <div
+              style={{ backgroundImage: `url(${"/hero.png"})` }}
+              className="relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1]"
+            ></div>
+          </div>
+
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : (
+              <ProductList title="Featured Products" items={products! ?? []} />
+            )}
+          </div>
+        </div>
+      </StorefrontLayout>
     </>
   );
 };
